74d1230a662c00aeba30427ef530c192a5b7ac90
Sonboth <linhardtdavid@gmail.com>
2022-02-25 09:32:33 +0100

    fmriprep_legacy layout bug

--- rundocker.sh
+++ rundocker.sh
@@ -6,4 +6,4 @@ docker run -ti --rm $1 \
 	-v $base_path/derivatives:/flywheel/v0/output  \
 	-v $base_path/BIDS:/flywheel/v0/BIDS  \
 	-v $base_path/config/prfprepare.json:/flywheel/v0/config.json \
-	davidlinhardt/prfprepare:latest
+	davidlinhardt/prfprepare:0.0.5
