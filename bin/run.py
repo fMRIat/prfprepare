5bc24610514209e043b5428eeb64c653cef0fb81
Sonboth <linhardtdavid@gmail.com>
2022-04-24 15:30:15 +0200

    output just one bold file and .jsons for ever atlas/roi combination indicated the vertices

--- bin/run.py
+++ bin/run.py
@@ -7,6 +7,7 @@ Created on Wed Apr 13 11:49:37 2022
 """
 
 import json, os, sys
+from os import path
 from neuropythy.commands import atlas
 import bids
 import glob as glob
@@ -14,7 +15,7 @@ import glob as glob
 # for the annots part
 import nibabel as nib
 import subprocess as sp
-from zipFile import ZipFile
+from zipfile import ZipFile
 import numpy as np
 
 # get all needed functions
@@ -25,8 +26,8 @@ from link_stimuli   import link_stimuli
 
 flywheelBase  = '/flywheel/v0'
 
-configFile = os.path.join(flywheelBase, 'config.json')
-bidsDir = os.path.join(flywheelBase, 'BIDS')
+configFile = path.join(flywheelBase, 'config.json')
+bidsDir = path.join(flywheelBase, 'BIDS')
 
 def die(*args):
     print(*args)
@@ -42,7 +43,7 @@ try:
     with open(configFile, 'r') as fl:
         conf = json.load(fl)
 except Exception:
-    die("Could not read config.json!")
+    die('Could not read config.json!')
     
 # subject from config and check
 sub   = conf['subject']
@@ -55,26 +56,33 @@ if not sub in BIDSsubs:
 note(f'Working on Subject: {sub}')
 
 # session if given otherwise it will loop through sessions from BIDS
+BIDSsess = layout.get(subject=sub, return_type='id', target='session') 
 if hasattr('conf', 'session'):
-    if conf['sessionName'][0] != 'all':
-        sess = conf['sessionName']
+    if conf['session'][0] == 'all':
+        sess =  BIDSsess
     else:
-        sess = layout.get(subject=sub, return_type='id', target='session')  
+        sess = conf['session']
 else:
-    sess = layout.get(subject=sub, return_type='id', target='session')  
+    sess = BIDSsess 
 
 # ROIs and atlases from config
-areas   = conf['rois'].split(']')[0].split('[')[-1].split(',')
-atlases = conf['atlases'].split(']')[0].split('[')[-1].split(',')
+areas   = conf['config']['rois'].split(']')[0].split('[')[-1].split(',')
+atlases = conf['config']['atlases'].split(']')[0].split('[')[-1].split(',')
 if atlases[0] == 'all':
     atlases = ['benson', 'wang']
 
 # Annotations
-fs_annot     = conf["fs_annot"]
+#!!! maybe we should define an atlas called fs_annot or fs_custom and 
+#    the IDs could go in the rois field in the config?
+#    Or are there other fs_annots possible?
+fs_annot     = conf['fs_annot']
 fs_annot_ids = conf['fs_annot_ids'].split(']')[0].split('[')[-1].split(',')
 
-if fs_annot=="custom.zip":
-    # If it is custom, it meand that there will be a file called custom.zip in fsaverage/label folder that we will need to convert to  individual subject space first. This means like Neuropythy, this step will update the subject's label folder with new files. 
+if fs_annot=='custom.zip':
+    # If it is custom, it meand that there will be a file called custom.zip in 
+    # fsaverage/label folder that we will need to convert to  individual subject 
+    # space first. This means like Neuropythy, this step will update the subject's 
+    # label folder with new files. 
     # Do it below after the dirs have been defined
     convert_custom_annot = True
 
@@ -89,21 +97,22 @@ else:
     forceParams = False
 
 # define input direcotry
-inDir = os.path.join(flywheelBase, 'input', f'analysis-{conf["config"]["fmriprep_analysis"]}')
+inDir = path.join(flywheelBase, 'input', f'analysis-{conf["config"]["fmriprep_analysis"]}')
+note(f'Loading data fraom {inDir}')
 
 # check the BIDS directory
-if not os.path.isdir(bidsDir):
+if not path.isdir(bidsDir):
     die('no BIDS directory found!')
 
 # define and check subject and freesurfer dir
 if conf['fmriprep_legacy_layout'] == True:
-    subInDir = os.path.join(inDir, 'fmriprep', f'sub-{sub}')
-    fsDir    = os.path.join(inDir, 'freesurfer')
+    subInDir = path.join(inDir, 'fmriprep', f'sub-{sub}')
+    fsDir    = path.join(inDir, 'freesurfer')
 else:
-    subInDir = os.path.join(inDir, f'sub-{sub}')
-    fsDir    = os.path.join(inDir, 'sourcedata', 'freesurfer')
+    subInDir = path.join(inDir, f'sub-{sub}')
+    fsDir    = path.join(inDir, 'sourcedata', 'freesurfer')
 
-if os.path.isdir(fsDir):
+if path.isdir(fsDir):
     note(f'Freesurfer dir found at {fsDir}!')
 else:
     die(f'No freesurfer dir found at {fsDir}!')
@@ -117,91 +126,105 @@ found_outbids_dir = False
 
 while not found_outbids_dir and analysis_number<100:
     analysis_number += 1
-    outDir=os.path.join(flywheelBase, 'output', 'prfprepare', f'analysis-{analysis_number:02d}')
-    optsFile = os.path.join(outDir, 'options.json')  
+    
+    outDir=path.join(flywheelBase, 'output', 'prfprepare', f'analysis-{analysis_number:02d}')
+    optsFile = path.join(outDir, 'options.json')  
+    
     # if the analyis-XX directory exists check for the config file
-    if os.path.isdir(outDir) and os.path.isfile(optsFile): 
+    if path.isdir(outDir) and path.isfile(optsFile): 
         with open(optsFile, 'r') as fl: 
             opts = json.load(fl)
+            
         # check for the options file equal to the config            
         if sorted(opts.items()) == sorted(conf['config'].items()): 
             found_outbids_dir = True
+            
     # when we could not find a fitting analysis-XX forlder we make a new one
     else:
-        if not os.path.isdir(outDir):
+        if not path.isdir(outDir):
             os.makedirs(outDir, exist_ok=True)
-            # dump the options file in the output directory
+            
+        # dump the options file in the output directory
         with open(optsFile, 'w') as fl:
-            json.dump(conf['config'], fl)    
+            json.dump(conf['config'], fl, indent=4)    
         found_outbids_dir = True
+        
 note(f'Output directory: {outDir}')
 
 # define the subject output dir
-subOutDir = os.path.join(outDir, f'sub-{sub}')
+subOutDir = path.join(outDir, f'sub-{sub}')
 
 
 ###############################################################################
 # run neuropythy if not existing yet
-if not os.path.isfile(os.path.join(fsDir, f'sub-{sub}', 'surf', 'lh.benson14_varea.mgz')):
+if not path.isfile(path.join(fsDir, f'sub-{sub}', 'surf', 'lh.benson14_varea.mgz')):
     try:
         print('Letting Neuropythy work...')
         os.chdir(fsDir)
         atlas.main(f'sub-{sub}')
-        os.chdir(os.path.expanduser("~"))
+        os.chdir(path.expanduser('~'))
     except:
-        print('Neuropythy failed!')
-        die()
+        die('Neuropythy failed!')
 
 #######################
 # Obtain the vertex numbers by combining the required Atlases and ROIs
 # Convert the custom annot from fsaverage to individual space as well
 if convert_custom_annot:
    # Unzip the annotations
-   fsavglbl = os.path.join(fsDir,"fsaverage","label")
-   fsavglblcustom = os.path.join(fsavglbl, "custom")
-   if not os.path.isdir(fsavglblcustom): os.mkdir(fsavglblcustom)  
-   with ZipFile(os.path.join(fsavglbl,"custom.zip"),'r') as zipObj:
-       zipObj.extractall(fsavglblcustom)
+   fsAvgLabel = path.join(fsDir,'fsaverage','label')
+   fsAvgLabelCustom = path.join(fsAvgLabel, 'custom')
+   fsAvgLabelCustomZip = path.join(fsAvgLabelCustom,'custom.zip')
+   
+   if not path.exists(fsAvgLabelCustomZip): 
+       die(f'We could not find the {fsAvgLabelCustomZip}')
+       
+   with ZipFile(fsAvgLabelCustomZip, 'r') as zipObj:
+       zipObj.extractall(fsAvgLabelCustom)
 
    # Read all the annotations
-   os.chdir(fsavglblcustom)
-   annots = glob.glob("*.annot")
+   os.chdir(fsAvgLabelCustom)
+   annots = glob.glob('*.annot')
   
-   # Create a dictionary to store all the annots, labels and its vertices
-   customAnnotDict = dict()
 
    # Convert the annots to indivisual subject space using surf2surf
-   os.environ["SUBJECTS_DIR"]=fsDir 
+   os.environ['SUBJECTS_DIR'] = fsDir 
+   
    # for ses in sess: # I think we need to fix the sess thing... we will talk about it :)
-   # sublbl = os.path.join(fsDir,f'sub-{sub}',f'ses-{ses}','label')
-   sublbl = os.path.join(fsDir,f'sub-{sub}','label')
+   # sublbl = path.join(fsDir,f'sub-{sub}',f'ses-{ses}','label')
+   sublbl = path.join(fsDir,f'sub-{sub}','label')
    for annot in annots:
-       hemi = annot.split(".")[0]
-       cmd = (f'mri_surf2surf --srcsubject fsaverage --trgsubject sub-{sub} --hemi {hemi} '  
-              f'--sval-annot {os.path.join(fsavglblcustom,annot)} --tval {sublbl}/{annot}')
-       sp.call(cmd, shell=True)   
-       # Now we need to extact every label from evey annot so that we can read the vertices to create the big ROI for the analysis
-       # I think we do not need to extract the labels, it is enough reading the vertices (I had to extract them for DWI, not here)
-       # I leave the extraction code here just in case, delete later. 
-            # Extract the individual labels
-            # --labelbase ${ANNOTname}
-            # mri_annotation2label --subject ${SUBJECT_ID} --hemi ${hemi}  \
-            #                      --annotation ${labeldir}/${ANNOTname}.annot --outdir $labeldirtmp
-       # Read the new annot:
-       customAnnotDict[annot] = nib.freesurfer.read_annot(f'{sublbl}/{annot}')
+       if not path.isfile(f'{sublbl}/{annot}'):
+           hemi = annot.split('.')[0]
+           cmd = (f'mri_surf2surf --srcsubject fsaverage --trgsubject sub-{sub} --hemi {hemi} '  
+                  f'--sval-annot {path.join(fsAvgLabelCustom,annot)} --tval {sublbl}/{annot}')
+           sp.call(cmd, shell=True)   
+           # Now we need to extact every label from evey annot so that we can read the vertices to create the big ROI for the analysis
+           # I think we do not need to extract the labels, it is enough reading the vertices (I had to extract them for DWI, not here)
+           # I leave the extraction code here just in case, delete later. 
+                # Extract the individual labels
+                # --labelbase ${ANNOTname}
+                # mri_annotation2label --subject ${SUBJECT_ID} --hemi ${hemi}  \
+                #                      --annotation ${labeldir}/${ANNOTname}.annot --outdir $labeldirtmp
+
             
 
 
-# David, here you have the code to read the labels and vertices:
-# annots == list(customAnnotDict.keys())
-for annot in annots:
-    subannot = customAnnotDict[annot]
-    for i,lab in enumerate(subannot[2]):
-        label          = lab.decode('UTF-8').strip("'")
-        vertices0based = np.argwhere(subannot[0]==i)
+# # Create a dictionary to store all the annots, labels and its vertices
+# customAnnotDict = dict()
+# # Read the new annot:   
+# annots = glob.glob(path.join(fsAvgLabelCustom, '*.annot'))
+# for annot in annots:
+#        customAnnotDict[annot] = nib.freesurfer.read_annot(f'{sublbl}/{annot}')
+# # David, here you have the code to read the labels and vertices:
+# # annots == list(customAnnotDict.keys())
+# for annot in annots:
+#     subannot = customAnnotDict[annot]
+#     for i,lab in enumerate(subannot[2]):
+#         label          = lab.decode('UTF-8').strip("'")
+#         vertices0based = np.argwhere(subannot[0]==i)
 
-# Here you have all teh annot > Label > vertices
-# I assume that you will sum up all the vertices so that we only analyze one vertex once right? 
+# # Here you have all teh annot > Label > vertices
+# # I assume that you will sum up all the vertices so that we only analyze one vertex once right? 
 
 
 
@@ -214,13 +237,19 @@ etcorr = stim_as_nii(sub, sess, bidsDir, subOutDir, etcorr, forceParams, force, 
 
 print('Masking data with visual areas and save them to 2D nifti...')
 nii_to_surfNii(sub, sess, layout, bidsDir, subInDir, subOutDir, fsDir, forceParams,
-               fmriprepLegacyLayout, atlases, areas, etcorr, force, verbose)
+               fmriprepLegacyLayout, atlases, areas, force, verbose)
+
+# run this again with the eyetracker correction if applicable
+if etcorr:
+    nii_to_surfNii(sub, sess, layout, bidsDir, subInDir, subOutDir.replace(f'analysis-{analysis_number:02d}',
+                                                                           f'analysis-{analysis_number:02d}_ET'), 
+                   fsDir, forceParams, fmriprepLegacyLayout, atlases, areas, force, verbose)
 # we could add some option for smoothing here?
 
 print('Creating events.tsv for the data containing the correct stimulus...')
 link_stimuli(sub, sess, layout, bidsDir, subOutDir, etcorr, force, verbose)
 
-os.chdir(os.path.expanduser("~"))
+os.chdir(path.expanduser('~'))
 
 
 # exit happily
