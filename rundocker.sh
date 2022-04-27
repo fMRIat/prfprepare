6370ad40b0550ec4eb5c3d3ca20e4cc9f5228619
Garikoitz <garikoitz@gmail.com>
2022-04-27 12:13:28 +0200

    there was small typo -v added after container name

--- rundocker.sh
+++ rundocker.sh
@@ -5,8 +5,7 @@ export cmd="docker run -ti --rm $4 \
 	         -v $2/BIDS/derivatives:/flywheel/v0/output  \
 	         -v $2/BIDS:/flywheel/v0/BIDS  \
 	         -v $2/$3:/flywheel/v0/config.json \
-	         davidlinhardt/prfprepare:$1 \
-	         -v"
+	         davidlinhardt/prfprepare:$1"
 echo "Launching the following command: "
 echo $cmd
 eval $cmd
