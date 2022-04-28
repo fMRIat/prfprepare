5c8582cc1e863736817bb21f2f1ba716b46b4025
Garikoitz <garikoitz@gmail.com>
2022-04-28 18:19:18 +0200

    updated version to 1.0.2

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 # 1.0.0 first version
 # 1.0.1 fixed averaging of different run lengths
 #       added option custom_output_name
-
+# 1.0.2 Removed Freesurfer license writing within  the machine because of FS's license and because Singularity does not allow writing within file system
 
 ME=davidlinhardt
 GEAR=prfprepare
-VERSION=1.0.1
+VERSION=1.0.2
 docker build --tag $ME/$GEAR:$VERSION .
 
